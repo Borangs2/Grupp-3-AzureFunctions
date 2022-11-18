d6762d5378ee1e3e08fd9a451b35e6b609daf518
Andreas Boräng <andbor02@gmail.com>
2022-11-18 13:44:54 +0100

    Added CreateComment

---
+++ AzureFunctions/Maui/Comments/CreateComment.cs
@@ -0,0 +1,54 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Grupp3_Elevator.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq.Expressions;
+using Dapper;
+
+namespace AzureFunctions.Maui.Comments
+{
+    public static class CreateComment
+    {
+        private static string DbConnectionString = "Server=tcp:kyh-devops.database.windows.net,1433;Initial Catalog=Kyh-Agile Grupp 3;Persist Security Info=False;User ID=CloudSA37b586b4;Password=Andreas1!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+
+        [FunctionName("CreateComment")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "comment/create")] HttpRequest req,
+            ILogger log)
+        {
+            CreateErrandCommentModel data;
+            try
+            {
+                data = JsonConvert.DeserializeObject<CreateErrandCommentModel>(
+                    await new StreamReader(req.Body).ReadToEndAsync());
+            }
+            catch
+            {
+                return new BadRequestResult();
+            }
+
+
+            if (data == null || data.Id == Guid.Empty)
+                return new BadRequestResult();
+
+
+            using IDbConnection connection = new SqlConnection(DbConnectionString);
+
+            var result = await connection.QueryAsync("INSERT INTO ErrandComments VALUES (@Id, @Content, @PostedAt, @ErrandId, @Author)", 
+                new {Id = data.Id, Content = data.Content, PostedAt = data.PostedAt, ErrandId = data.ErrandId, Author = data.Author});
+
+            if(result != null )
+                return new OkObjectResult(data);
+            return new BadRequestResult();
+        }
+    }
+}
---
+++ AzureFunctions/Maui/Elevator/Models/CreateCommentModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Grupp3_Elevator.Models;
+
+public class CreateErrandCommentModel
+{
+    public Guid Id { get; set; }
+    public string Content { get; set; }
+    public Guid? Author { get; set; }
+    public DateTime PostedAt { get; set; }
+    public Guid ErrandId { get; set; }
+
+    public CreateErrandCommentModel(Guid id, string content, string author, DateTime postedAt, Guid errandId)
+    {
+        Id = id;
+        Content = content;
+        if (Guid.TryParse(author, out Guid authorGuid))
+            Author = authorGuid;
+        else
+            Author = null;
+        PostedAt = postedAt;
+        ErrandId = errandId;
+    }
+}
