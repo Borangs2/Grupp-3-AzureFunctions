e57b678dd30bb8eae60cb3a44b658c2f5be088f5
Andreas Boräng <andbor02@gmail.com>
2022-12-06 10:50:48 +0100

    Implemented the Helper class

--- AzureFunctions/Models/ElevatorDetailedModel.cs
+++ AzureFunctions/Models/ElevatorDetailedModel.cs
@@ -1,25 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using AzureFunctions.Models.Interfaces;
 
 namespace AzureFunctions.Models;
 
-public class ElevatorDetailedModel
+public class ElevatorDetailedModel : IElevator
 {
-    public enum ElevatorStatus
+    public ElevatorDetailedModel()
     {
-        Disabled /*Elevator off, doors closed*/,
-        Idle /*Elevator on, doors closed, not running*/,
-        Running /*Elevator on, doors closed, running*/,
-        Error /*Elevator error*/
+        
     }
-
     public Guid Id { get; set; } = Guid.NewGuid();
     public string ConnectionString { get; set; }
     public string Name { get; set; } = "";
-    public ElevatorStatus Status { get; set; } = ElevatorStatus.Disabled;
+    public IElevator.ElevatorStatus Status { get; set; } = IElevator.ElevatorStatus.Disabled;
     public bool DoorStatus { get; set; } = false;
     public int CurrentLevel { get; set; } = 0;
     public int TargetLevel { get; set; } = 0;
 
-    public List<ErrandModel> Errands { get; set; }
+    public List<ErrandModel> Errands { get; set; } = new List<ErrandModel>();
 }
